0c2b82d5f9ddb9abd9e521f12faae55e06599a00
tavora <tarikly+github@gmail.com>
2024-04-25 09:42:51 -0300

    make apps to listen to all addresses

--- back/index.js
+++ back/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from the backend service!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend service is running on port ${PORT}`);
 });
